12262504bda8d9a1e559e9afa4ba55c0c64f2ec8
Nahid Fahim <fahimbinimam@gmail.com>
2025-07-22 14:58:42 +0600

    create Student table

---
+++ src/StudentDAO.java
@@ -0,0 +1,20 @@
+import java.sql.*;
+
+public class StudentDAO {
+    //create table if not existed
+    public void createTable() throws SQLException{
+        String sql = "CREATE TABLE IF NOT EXISTS students(" +
+                "id INT AUTO_INCREMENT PRIMARY KEY, " +
+                "name VARCHAR(100) NOT NULL, " +
+                "email VARCHAR(100) UNIQUE NOT NULL, " +
+                "department VARCHAR(50))";
+
+        try (Connection conn = DatabaseConnection.getConnection();
+        Statement stmt = conn.createStatement()){
+            stmt.execute(sql);
+        }
+    }
+
+
+
+}
