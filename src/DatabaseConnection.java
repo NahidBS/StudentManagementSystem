5ebacb3bb1c1b8fd5370cf875c8b92c632ec467e
Nahid Fahim <fahimbinimam@gmail.com>
2025-07-22 12:37:23 +0600

    added databse connection

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,3 +1,34 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.*;
+
 public class DatabaseConnection {
-private class static final String URL = ""
+    private static final String URL = "jdbc:mysql://localhost:3306/student_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "password";
+    private static Connection connection;
+
+    private DatabaseConnection() {}
+    private static synchronized Connection getConnection () throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            } catch (ClassNotFoundException e) {
+                throw new SQLException("Database driver not found", e);
+            }
+        }
+        return connection;
+    }
+
+    public static void closeConnection () {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                    connection.close();
+            }
+        }catch (SQLException e) {
+            System.err.println("Error closing connection: " + e.getMessage());
+        }
+    }
 }
+
